14d095328e67dfdbf7ac1a6ff746cfdd2fc3dc3a
Shivam Bhargava <shivamb@live.unc.edu>
2021-08-31 22:19:10 -0400

    Finished EX01 #1

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,4 +1,7 @@
-# TODO: Write docstring here
+name: str = input("What is your name? ")
+print("Oh my goodness " + name + " look at you go!")
+print(name + " I am so proud of you")
+print("Kick today in the butt " + name)
 
 # TODO: Initialize __author__ variable here
 
