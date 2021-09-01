e3e4c56dedfc7743df1adb535331d5a126c9a892
Shivam Bhargava <shivamb@live.unc.edu>
2021-08-31 23:51:42 -0400

    Progress on Exercise 1

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,8 +1,8 @@
+"""Hype sentences created uing input and strings."""
+# TODO: Initialize __author__ variable here
+__author__: str = "730241536"
+# TODO: Implement your program logic here
 name: str = input("What is your name? ")
 print("Oh my goodness " + name + " look at you go!")
 print(name + " I am so proud of you")
 print("Kick today in the butt " + name)
-
-# TODO: Initialize __author__ variable here
-
-# TODO: Implement your program logic here
